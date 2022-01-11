348a3243efafd7809aed5915a532460851302d96
Alejandro Gomez Morales <gomez.morales.alejandro@gmail.com>
2022-01-11 14:28:04 -0600

    Fin Seccion 10 -Auth

--- index.js
+++ index.js
@@ -13,23 +13,17 @@ const app = express();
 //configutar CORS
 app.use(cors());
 
+//Lectura y parseo del Body
+app.use(express.json());
+
 //base de Datos
 dbConnection();
-
-
-
 //mean_user
 //S6TiM7ZJQtwUEPsr
+
 //Rutas
-app.get('/', (req, res) => {
-    res.json({
-
-        ok: true,
-        msg: 'Hola Mundo'
-
-    })
-
-})
+app.use('/api/usuarios', require('./routes/usuarios'));
+app.use('/api/login', require('./routes/auth'));
 
 app.listen(process.env.PORT, () => {
     console.log('Servidor corriendo en puerto ' + process.env.PORT);
